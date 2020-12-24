d25bf7c300019763c323dae1f7a5e26f2d30c845
Denis Grafov <grafov.denis@gmail.com>
2020-12-24 22:02:06 +0300

    add photo and sliver

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,13 +8,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(Icons.nights_stay),
-            onPressed: () => BlocProvider.of<ThemeCubit>(context).switchTheme(),
-          )
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, scrolled) {
+          return <Widget>[
+            SliverAppBar(
+              elevation: 0,
+              expandedHeight: 300,
+              floating: false,
+              pinned: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.nights_stay),
+                  onPressed: () =>
+                      BlocProvider.of<ThemeCubit>(context).switchTheme(),
+                  color: (Theme.of(context).brightness == Brightness.light)
+                      ? Colors.black
+                      : Colors.white,
+                )
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text("Denis Grafov"),
+                background: Image.asset(
+                  'assets/images/profile.jpg',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            )
+          ];
+        },
+        body: Container(),
       ),
     );
   }
